fc6eb988fb9089515d5025773066540c514529dd
Tim Traylor <ttraylor83@gmail.com>
2022-12-06 05:14:56 +0000

    latest

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import { useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
@@ -7,42 +5,34 @@ import FeedbackData from './data/FeedbackData';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackFrom from './components/FeedbackForm';
 import AboutPage from './pages/About';
+import { FeedbackProvider } from './context/FeedbackContext';
 import AboutIconLink from './components/AboutIconLink';
-import Post from './components/Post';
+// import Post from './components/Post';
 
 const App = () => {
-  const [feedback, setFeedback] = useState(FeedbackData);
-
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4();
-    console.log('in addFeedback', newFeedback);
-    setFeedback([newFeedback, ...feedback]);
-  };
-
-  const deleteFeedback = (id) => {
-    setFeedback(feedback.filter((fb) => fb.id !== id));
-  };
 
   return (
-    <Router>
-      <Header />
-      <div className='container'>
-        <Routes>
-          <Route exact path='/' element={
-            <>
-              <FeedbackFrom handleAdd={addFeedback} />
-              <FeedbackStats feedback={feedback} />
-              <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
-            </>
-          } />
-         
-          <Route path='/about' element={<AboutPage />} />
-          <Route path='/post/*' element={<Post />} />
-          {/* <Route path='/post/:id/:name' element={<Post />} /> */}
-        </Routes>
-        <AboutIconLink />
-      </div>
-    </Router>
+    <FeedbackProvider>
+      <Router>
+        <Header />
+        <div className='container'>
+          <Routes>
+            <Route exact path='/' element={
+              <>
+                <FeedbackFrom />
+                <FeedbackStats />
+                <FeedbackList />
+              </>
+            } />
+          
+            <Route path='/about' element={<AboutPage />} />
+            {/* <Route path='/post/*' element={<Post />} /> */}
+            {/* <Route path='/post/:id/:name' element={<Post />} /> */}
+          </Routes>
+          <AboutIconLink />
+        </div>
+      </Router>
+    </FeedbackProvider>
   );
 };
 
--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,23 +1,28 @@
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
 
-const FeedbackStats = ({feedback}) => {
+import { useContext } from 'react';
+import FeedbackContext from '../context/FeedbackContext';
 
-  let calcAvg = feedback.reduce( (acc, cur) => {
-    return acc + cur.rating
-  }, 0) / feedback.length
+const FeedbackStats = () => {
+  const { feedback } = useContext(FeedbackContext);
 
-  calcAvg = calcAvg.toFixed(1).replace(/[.,]0$/, '')
+  let calcAvg =
+    feedback.reduce((acc, cur) => {
+      return acc + cur.rating;
+    }, 0) / feedback.length;
+
+  calcAvg = calcAvg.toFixed(1).replace(/[.,]0$/, '');
 
   return (
     <div className='feedback-stats'>
       <h4>{feedback.length} Reviews</h4>
       <h4>Average Rating: {isNaN(calcAvg) ? 0 : calcAvg}</h4>
     </div>
-  )
-}
+  );
+};
 
-FeedbackStats.propTypes = {
-  feedback: PropTypes.array.isRequired,
-}
+// FeedbackStats.propTypes = {
+//   feedback: PropTypes.array.isRequired,
+// }
 
-export default FeedbackStats
+export default FeedbackStats;
